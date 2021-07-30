d4b9dba5ee80d64a39c379526f1f50ca2e7e8304
kausav <kausavkumar23@gmail.com>
2021-07-30 13:49:19 +0530

    exp1-3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ "bye!bye" });
 });
 
 const PORT = process.env.PORT || 5000;
